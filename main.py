ccf09dacefaef8140d38078c5d62efa02826a8cd
ivmanikhin <ivmanikhin@gmail.com>
2021-11-10 20:57:20 +0300

    now it works

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import pandas as pd
 import turtle
+from state_on_map import StateOnMap
 
 screen = turtle.Screen()
 screen.title('50 states quiz')
 screen.setup(800, 600)
+screen.tracer(0)
 us_map = 'blank_states_img.gif'
 screen.addshape(us_map)
 turtle.shape(us_map)
 states = pd.read_csv('50_states.csv')
-state_names = states.state.to_list()
+state_names_original = states.state.to_list()
+state_names = [item.lower() for item in state_names_original]
+states_x = states.x.to_list()
+states_y = states.y.to_list()
+states_x = [int(x) for x in states_x]
+states_y = [int(y) for y in states_y]
 print(state_names)
+states_on_map = []
+screen.tracer(0)
+screen.update()
 while len(state_names) > 0:
-    answer = screen.textinput('Guess the State', "What's another state's name?").capitalize()
+    try:
+        answer = screen.textinput('Guess the State', "What's another state's name?").lower()
+        print(answer)
+    except AttributeError:
+        break
     if answer in state_names:
-        state_names.pop(state_names.index(answer))
-        print(len(state_names))
+        state_index = state_names.index(answer)
+        state_names[state_index] = ''
+        states_on_map.append(StateOnMap(state_names_original[state_index], (states_x[state_index], states_y[state_index])))
+        screen.update()
 
 turtle.mainloop()
--- state_on_map.py
+++ state_on_map.py
@@ -1,1 +1,11 @@
 from turtle import Turtle
+
+
+class StateOnMap(Turtle):
+
+    def __init__(self, name, coord):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(coord)
+        self.write(name, move=False, align='center', font=('Arial', 10, 'normal'))
